824f0503b87bb4998727f3dda6a47853dcb6a332
‘JunHooo’ <2423026235@qq.com>
2022-02-22 15:42:56 +0800

    创建404页面

---
+++ src/components/LayoutCom.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <slot></slot>
+  <Nav></Nav>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "LayoutCom"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+<div>404
+  <router-link to="/">返回首页</router-link>
+<!--  //使用不带#号的会刷新一次页面-->
+<!--  <a href="/">返回首页</a>-->
+<!--  <a href="#/">返回首页</a>-->
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
