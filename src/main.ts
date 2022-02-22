83d6623c431bd870666cdffc9d59282f10d2593c
‘JunHooo’ <2423026235@qq.com>
2022-02-22 16:00:29 +0800

    关闭eslint对文件名的校验。

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import Nav from '@/components/NavCom.vue';
-import Layout from '@/components/LayoutCom.vue'
+import Nav from '@/components/Nav.vue';
+import Layout from '@/components/Layout.vue'
 
 
 Vue.config.productionTip = false
 
-// eslint-disable-next-line vue/multi-word-component-names
 Vue.component('Nav',Nav)
-// eslint-disable-next-line vue/multi-word-component-names
 Vue.component('Layout',Layout)
 new Vue({
   router,
