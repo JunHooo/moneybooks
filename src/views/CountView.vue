b0a57a0b30e32eb7db243e74ea3241591aab5870
‘JunHooo’ <2423026235@qq.com>
2022-02-22 15:49:10 +0800

    实现Count页面布局。

--- src/views/CountView.vue
+++ src/views/CountView.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <Layout>
-      CountView
-    </Layout>
+  <div class="nav-wrapper">
+      <div class="content">CountView</div>
+      <Nav></Nav>
   </div>
 </template>
 
@@ -12,5 +11,17 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper{
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    //当内容超过页面高度时，让导航栏不往下滑
+    overflow: auto;
+    height:100vh ;
+  }
+  .content{
+    //占据剩下所有页面
+    flex-grow: 1;
+  }
 
 </style>
