c39eac7a2d5c1d6a32cc28ed5e036311ff79ae41
‘JunHooo’ <2423026235@qq.com>
2022-02-22 15:55:27 +0800

    使用插槽完成所有页面布局。

--- src/components/LayoutCom.vue
+++ src/components/LayoutCom.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-  <slot></slot>
+<div class="nav-wrapper">
+  <div class="content"><slot></slot></div>
   <Nav></Nav>
 </div>
 </template>
@@ -12,5 +12,16 @@ name: "LayoutCom"
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height:100vh ;
+}
+.content{
+  //当内容超过页面高度时，让导航栏不往下滑
+  overflow: auto;
+  //占据剩下所有页面
+  flex-grow: 1;
+}
 </style>
--- src/views/CountView.vue
+++ src/views/CountView.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="nav-wrapper">
-      <div class="content">CountView</div>
-      <Nav></Nav>
+  <div>
+    <Layout><p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+      <p>CountView</p>
+    </Layout>
   </div>
 </template>
 
@@ -11,17 +50,6 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper{
-    border: 1px solid red;
-    display: flex;
-    flex-direction: column;
-    //当内容超过页面高度时，让导航栏不往下滑
-    overflow: auto;
-    height:100vh ;
-  }
-  .content{
-    //占据剩下所有页面
-    flex-grow: 1;
-  }
+
 
 </style>
