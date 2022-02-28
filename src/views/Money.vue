4b2b498ddbd50af199f0aa4809b9292ea42f0fd6
‘JunHooo’ <2423026235@qq.com>
2022-02-28 15:40:46 +0800

    Money.vue引入Tabs。

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad @update:value="onUpdateMoney" @submit="saveRecord"/>
-    <Types @update:value="onUpdateType"></Types>
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
     <Notes field-name="备注" placeholder="在这里输入备注"
            @update:value="onUpdateNotes"/>
     <Tags @update:value="onUpdateTags"/>
@@ -11,34 +12,35 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
+import Tabs from '@/components/Tabs.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component,} from 'vue-property-decorator';
 import store from '@/store/index2';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tags, Notes, Types, NumberPad},
+  components: {Tags, Notes, Tabs, NumberPad},
 })
 export default class Money extends Vue {
+  recordTypeList = recordTypeList;
   recordList = store.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', money: 0,
   };
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
-  }
-
-  onUpdateType(value: string) {
-    this.record.type = value;
   }
 
   onUpdateMoney(value: string) {
     this.record.money = parseFloat(value);
   }
+
   saveRecord() {
     store.createRecord(this.record);
   }
